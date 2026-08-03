52e9e1d8513abee60512e4c074c7c83729df0c53
nazmi02551 <52427443+nazmi02551@users.noreply.github.com>
2026-08-03 22:37:06 +0300

    Add reversible experience presentation config

---
+++ apps/mobile/lib/core/config/experience_presentation_config.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+enum DecisionJourneyPresentationMode { progressive, legacyLongScroll }
+
+enum OnboardingExperienceVersion { v2, legacyV1 }
+
+class ExperiencePresentationConfig {
+  const ExperiencePresentationConfig({
+    required this.decisionJourneyMode,
+    required this.onboardingVersion,
+  });
+
+  factory ExperiencePresentationConfig.fromEnvironment() {
+    const progressiveDecisionJourney = bool.fromEnvironment(
+      'KEFE_PROGRESSIVE_DECISION_JOURNEY',
+      defaultValue: true,
+    );
+    const onboardingV2 = bool.fromEnvironment(
+      'KEFE_ONBOARDING_V2',
+      defaultValue: true,
+    );
+
+    return const ExperiencePresentationConfig(
+      decisionJourneyMode: progressiveDecisionJourney
+          ? DecisionJourneyPresentationMode.progressive
+          : DecisionJourneyPresentationMode.legacyLongScroll,
+      onboardingVersion: onboardingV2
+          ? OnboardingExperienceVersion.v2
+          : OnboardingExperienceVersion.legacyV1,
+    );
+  }
+
+  final DecisionJourneyPresentationMode decisionJourneyMode;
+  final OnboardingExperienceVersion onboardingVersion;
+}
+
+final experiencePresentationConfigProvider = Provider<ExperiencePresentationConfig>(
+  (ref) => ExperiencePresentationConfig.fromEnvironment(),
+);
